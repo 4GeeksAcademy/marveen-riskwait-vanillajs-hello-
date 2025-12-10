cb686c1ecd87633afb6724c4924baf3239cb3eec
Marveen Riskwait <radiostation87.7.7@gmail.com>
2025-12-10 14:37:29 +0000

    Proyecto Terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+function log_random_item (a, b){
+  return Math.floor(Math.random()* (a - b) + b);
+};
+
+function getRandomItem(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        throw new Error("Le paramètre doit être un tableau non vide.");
+    }
+    const index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
+let Who = getRandomItem(who);
+let Action = getRandomItem(action);
+let What= getRandomItem(what);
+let When= getRandomItem(when);
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector ('#excuse').innerHTML = (Who + " " + Action + " " + What + " " + When);
 };
+
+
